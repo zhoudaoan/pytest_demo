f919e67c06803d24bc077125ea6c64c2c5dff345
huahua <18225550463@163.com>
2021-05-16 22:24:12 +0800

    增加assert校验方式

--- read_json.py
+++ read_json.py
@@ -6,7 +6,7 @@ def read_json():
         data = json.load(f)
         return data
 
-#对比工具json
+#对比俩个json属于包含关系
 def cmp_dict(src_data,dst_data):
     assert type(src_data) == type(dst_data),"type: '{}' != '{}'".format(type(src_data), type(dst_data))
     if isinstance(src_data,dict):
@@ -18,6 +18,22 @@ def cmp_dict(src_data,dst_data):
             cmp_dict(src_list, dst_list)
     else:
         assert src_data == dst_data,"value '{}' != '{}'".format(src_data, dst_data)
+
+#对比两个json属于相等关系
+def cmp_dict2(src_data,dst_data):
+    assert type(src_data) == type(dst_data),"type: '{}' != '{}'".format(type(src_data), type(dst_data))
+    if isinstance(src_data,dict):
+        assert len(src_data) == len(dst_data),"dict len: '{}' != '{}'".format(len(src_data), len(dst_data))
+        for key in src_data:
+            assert dst_data.has_key(key)
+            cmp_dict(src_data[key],dst_data[key])
+    elif isinstance(src_data,list):
+        assert len(src_data) == len(dst_data),"list len: '{}' != '{}'".format(len(src_data), len(dst_data))
+        for src_list, dst_list in zip(sorted(src_data), sorted(dst_data)):
+            cmp_dict(src_list, dst_list)
+    else:
+        assert src_data == dst_data,"value '{}' != '{}'".format(src_data, dst_data)
+
 
 if __name__ == "__main__":
     # print(read_json())
